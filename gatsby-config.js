68ccfe27aef2ebda7a8a5e290d5543fa2be9761c
Mike Fennemore <mfennemore@schubergphilis.com>
2021-07-06 16:42:39 +0200

    Adds image

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,16 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-plugin-sass',
-    `gatsby-plugin-material-ui`
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'img'
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
   ],
   siteMetadata: {
     title: 'The Story of Beep',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,59 @@ import Header from '../components/Header';
 import Card from '@material-ui/core/Card';
 import { CardHeader, CardContent } from "@material-ui/core";
 import { ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Img from 'gatsby-image';
 import theme from "../theme";
+import { graphql } from "gatsby";
 
 const sections = [
   { title: 'Home', url: '/' },
   { title: 'About', url: '/about' }
 ];
 
-export default () => (
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+export default function BeepStory({data}) {
+  const classes = useStyles();
+  return (
   <ThemeProvider theme={theme}>
   <Container>
     <Header title="The Story of Beep" sections={sections} />
-    <Grid item xs={12} md={8}></Grid>
+    <div className={classes.heroContent}>
+    <Grid item xs={12} md={8}>
+      <Img fluid={data.file.childImageSharp.fluid} alt="logo"/>
+    </Grid>
+    </div>
     <Grid container spacing={2}>
       <Grid item xs={4}>
         <Card>
@@ -40,3 +81,16 @@ export default () => (
   </Container>
   </ThemeProvider>
 )
+};
+
+export const pageQuery = graphql`
+  query {
+    file(relativePath: { eq: "main.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1240) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
